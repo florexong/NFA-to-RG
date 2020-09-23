b43ba3070b716f97a1770508379f3de94eea68ed
Florex Ong <florexong99@gmail.com>
2020-09-23 21:08:03 +0800

    Revert "Update RegularToNFA.js"
    This reverts commit 5d4c69a83279b3224477d8e8ecb98776523bb165.

--- js/RegularToNFA.js
+++ js/RegularToNFA.js
@@ -1,60 +1,65 @@
 // append row to the HTML table
 function appendRow() {
-    var root=document.getElementById('rgTable').getElementsByTagName('tbody')[0];
-    var rows=root.getElementsByTagName('tr');
-    var clone=cloneEl(rows[rows.length-1]);
-    root.appendChild(clone);
-	
+    var tbl = document.getElementById('rgTable'), // table reference
+        row = tbl.insertRow(tbl.rows.length),      // append table row
+        i;
+		
     // insert table cells to the new row
-  //   for (i = 0; i < tbl.rows[0].cells.length; i++) {
-  //       var td = document.createElement('td');
-		// td = row.insertCell(i);
-		// if(i == 0)
-		// {
-		// 	var newCell = row.insertCell(i);
-		// 	newCell.innerHTML = "New State(s)";
-		// 	//createCell(row.insertCell(i), "New State", 'row');
-		// }
-		// else
-		// {
-		// 	var ele = document.createElement('input');
-		// 	ele.setAttribute('type', 'text');
-		// 	ele.setAttribute('value', '');
-		// 	td.appendChild(ele);
-		// }
-		// //
-  //   }
+    for (i = 0; i < tbl.rows[0].cells.length; i++) {
+        var td = document.createElement('td');
+		td = row.insertCell(i);
+		if(i == 0)
+		{
+			var newCell = row.insertCell(i);
+			newCell.innerHTML = "New State(s)";
+			//createCell(row.insertCell(i), "New State", 'row');
+		}
+		else
+		{
+			var ele = document.createElement('input');
+			ele.setAttribute('type', 'text');
+			ele.setAttribute('value', '');
+			td.appendChild(ele);
+		}
+		//
+    }
 }
  
 // create DIV element and append to the table cell
 function createCell(cell, text, style) {
     
 	cell.innerHTML = text;
-
+	//var div = document.createElement('div'), // create DIV element
+    //    txt = document.createTextNode(text); // create text node
+    //div.appendChild(txt);                    // append text node to the DIV
+    //div.setAttribute('class', style);        // set DIV class attribute
+    //div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+    //cell.appendChild(div);                   // append DIV to the table cell
 }
 
 // append column to the HTML table
 function appendColumn() {
-    var rows = document.getElementById('rgTable').getElementsByTagName('tr'), // table reference
-    i=0,r,c,clone;
+    var tbl = document.getElementById('rgTable'), // table reference
+        i;
     // open loop for each row and append cell
-    while (r=rows[i++])
+    for (i = 0; i <= tbl.rows.length; i++)
 	{
-		c =r.getElementsByTagName('td');
-        clone=cloneEl(c[c.length-1]);
-        c[0].parentNode.appendChild(clone);
-		// var td = document.createElement('td');
-		// td = tbl.rows[i].insertCell(tbl.rows[i].cells.length);
-		// var ele = document.createElement('input');
-		// ele.setAttribute('type', 'text');
-		// ele.setAttribute('value', 'T');
-		// td.appendChild(ele);
-		
+		var td = document.createElement('td');
+		td = tbl.rows[i].insertCell(tbl.rows[i].cells.length);
+		if(i == 0)
+		{
+			//var newCell = tbl.rows[i].insertCell(tbl.rows[i].cells.length);
+			//newCell.innerHTML = "New Alphabet";
+			//createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), "Cells", 'col');
+		}
+		else
+		{
+			var ele = document.createElement('input');
+			ele.setAttribute('type', 'text');
+			ele.setAttribute('value', 'T');
+			td.appendChild(ele);
+		}
 		
         //
     }
-}
-function cloneEl(el){
-    var clo=el.cloneNode(true);
-    return clo;
 }
