ce10a0de6ccb9ca5bb8dd3df666a7bd2b9aaa992
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2020-09-27 01:30:24 +0800

    Updated RG
    - Failed to add New Alphabet for each States
    - Added Alphabet

--- js/RegularToNFA.js
+++ js/RegularToNFA.js
@@ -4,12 +4,21 @@ function displayStates(tableID)
 
 arrayBigAlpha = 1;
 arraySmallAlpha = 1;
+arrayButton = 1;
+arrayContainer = 1;
+arrayContainerStates = 1;
 /***
 	Adding Alphabet and States for the States
 	Increment Alphabet based on [arraySmallAlpha]
 ***/
-function addAlphabet()
+function addAlphabet(containerID)
 {
+	
+	//console.log(containerID);
+	
+	var testdata = document.getElementById(containerID);
+	console.log(testdata);
+	
 	arraySmallAlpha += 1;
 	
 	var getAlphabet = document.createElement("INPUT");
@@ -20,25 +29,89 @@ function addAlphabet()
 	var getStates = document.createElement("INPUT");
 	getStates.setAttribute("type", "text");
 	getStates.setAttribute("size", "1");
-	getStates.setAttribute("id", "bigAlphabet"+arraySmallAlpha);
+	getStates.setAttribute("id", "bigAlphabet"+arrayBigAlpha);
 	
-	document.getElementById('alphabetContainer').appendChild(getAlphabet);
-	document.getElementById('alphabetContainer').appendChild(getStates);
+	var nodeName = document.createElement(nodeName);
+	nodeName.innerHTML = " | ";
+	
+	document.getElementById(containerID).appendChild(getAlphabet);
+	document.getElementById(containerID).appendChild(getStates);
+	document.getElementById(containerID).appendChild(nodeName);
 	//document.getElementById('alphabetContainer').appendChild(" | ");
+	
 }
 
 /***
 	Adding States
 	Increment States based on [arrayBigAlpha]
 ***/
-function addState()
+function addState(containID)
 {
+	
+	console.log(containID);
+	
 	arrayBigAlpha += 1;
-	var getData = document.createElement("INPUT");
-	getData.setAttribute("type", "text");
-	getData.setAttribute("size", "1");
-	getData.setAttribute("id", "bigAlphabet"+arrayBigAlpha);
-	document.getElementById('alphabetContainer').appendChild(getData);
+	
+	//var newLine = document.createElement('tr');
+	
+	var getStates = document.createElement("INPUT");
+	getStates.setAttribute("type", "text");
+	getStates.setAttribute("size", "1");
+	getStates.setAttribute("id", "bigAlphabet"+arrayBigAlpha);
+	
+	var string = document.createElement(nodeName);
+	string.innerHTML = " => ";
+	
+	var getAlphabet = document.createElement("INPUT");
+	getAlphabet.setAttribute("type", "text");
+	getAlphabet.setAttribute("size", "1");
+	getAlphabet.setAttribute("id", "smallAlphabet"+arraySmallAlpha);
+	
+	var nodeName = document.createElement(nodeName);
+	nodeName.innerHTML = " ";
+	
+	arrayBigAlpha += 1;
+	
+	var getStates2 = document.createElement("INPUT");
+	getStates2.setAttribute("type", "text");
+	getStates2.setAttribute("size", "1");
+	getStates2.setAttribute("id", "bigAlphabet"+arrayBigAlpha);
+	
+	var data = document.createElement(data);
+	data.innerHTML = " | ";
+	
+	arrayContainer += 1;
+	
+	var newContainers = document.createElement('span');
+	newContainers.setAttribute("id", "alphabetContainer"+arrayContainer);
+	
+	arrayButton += 1;
+	
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "Add Alphabet";
+	btn.setAttribute("type", "button");
+	btn.setAttribute("id", "button"+arrayButton);
+	btn.setAttribute("onclick", addAlphabet('"alphabetContainer"+arrayContainer'));
+	
+	var newLines = document.createElement('br');
+	
+	arrayContainerStates += 1;
+	
+	var newContainer = document.createElement('span');
+	newContainer.setAttribute("id", "stateContainer"+arrayContainerStates);
+	
+	//document.getElementById('stateContainer').appendChild(newLine);
+	document.getElementById(containID).appendChild(getStates);
+	document.getElementById(containID).appendChild(string);
+	document.getElementById(containID).appendChild(getAlphabet);
+	document.getElementById(containID).appendChild(nodeName);
+	document.getElementById(containID).appendChild(getStates2);
+	document.getElementById(containID).appendChild(data);
+	document.getElementById(containID).appendChild(newContainers);
+	document.getElementById(containID).appendChild(btn);
+	document.getElementById(containID).appendChild(newLines);
+	document.getElementById(containID).appendChild(newContainer);
+	//document.getElementById('stateContainer').appendChild(" | ");
 }
 
 bigAlphabets = 1;
