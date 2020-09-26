65b0767339f8e6d6b8eefdff9f8bcf9851bf4bf7
Arun Prasath <arunprasath218@gmail.com>
2020-09-26 19:44:51 +0800

    Completed NFA TO RG

--- js/NFAToRegular.js
+++ js/NFAToRegular.js
@@ -1,36 +1,101 @@
+function checkStringFunction()
+{
+
+
+}
 var alphabetValue = [];
 var stateValue = [];
 	
 function convertToRG()
 {
+	document.getElementById('RegularGrammer').innerHTML = '';
     var tbody = '';
     for( var i=0; i<stateNum;i++)
     {
-		tbody += stateValue[i] + '<span style="font-size:30px;">&#8594;</span>' 
+		var inputStateArray = [];
+		var inputStateCorrespondingAlphabet = [];
         for( var j=0; j<alphabetNum+1;j++)
         {
 			if(j==0){
-				//var tempValue = document.getElementById("s"+(i+1)).value;
-				//stateValue.push(tempValue);
+
 			}
 			else{
-				var tempValue = document.getElementById("tableInput"+ i + j).value;
-				if (tempValue!='∅')
+				var inputState = document.getElementById("tableInput"+ i + j).value;
+
+				if (inputState!='∅' && inputState!='')
 				{
-					tbody += alphabetValue[j-1] + tempValue + ' | ';
+					//tbody += alphabetValue[j-1] + inputState + ' | ';
+					inputStateCorrespondingAlphabet.push(alphabetValue[j-1]);
+					inputStateArray.push(inputState);
 				}
 			}
         }
+		
+		if(inputStateArray.length!=0)
+		{
+			tbody += stateValue[i] + '<span style="font-size:30px;">&#8594;</span>';
+			if(inputStateCorrespondingAlphabet[0]=='e')
+			{
+				tbody += inputStateArray[0];
+			}
+			else
+			{
+				tbody += inputStateCorrespondingAlphabet[0] + inputStateArray[0];
+			}
+		}
+		
+		for( var x=1; x<inputStateArray.length;x++){
+			if(inputStateCorrespondingAlphabet[x]=='e')
+			{
+				tbody += ' | ' + inputStateArray[x];
+			}
+			else
+			{
+				tbody += ' | ' + inputStateCorrespondingAlphabet[x] + inputStateArray[x];
+			}
+		}
+		
+		if((finalStateValue.includes(stateValue[i])) && inputStateArray.length==0)
+		{
+			tbody += stateValue[i] + '<span style="font-size:30px;">&#8594;</span> ε';
+		}
+		else if (finalStateValue.includes(stateValue[i]))
+		{
+			tbody += ' | ε';
+		}
+		
         tbody += '<br>';
     }
+	
     document.getElementById('RegularGrammer').innerHTML = tbody;
+	
+	var checkStringbody = '<table border="1"><td>';
+
+	checkStringbody += 'Check strings (input)<br>'
+	
+	checkStringbody +='<input type="text" id="checkString1"><br><div id="checkStringContainer"></div><button type="button" onclick="appendCheckStringInput()">+</button><br></td><td>'
+	
+	checkStringbody += '<button type="button" onclick="checkStringFunction()">Check strings</button><br>'
+	
+	checkStringbody += '</td></table>'
+	
+	document.getElementById('CheckString').innerHTML = checkStringbody;
+	
 }
 
-
+var startStateValue ='';
+var finalStateValue = [];
 function createTable()
 {
 
-
+	startStateValue = document.getElementById("startState").value;
+	
+	for(var i=0; i<finalStateNum; i++)
+	{
+		var tempValue = document.getElementById("finalState"+(i+1)).value;
+		finalStateValue.push(tempValue);
+	}
+	
     var theader = '<table border="1">\n';
     var tbody = '';
 	
@@ -52,7 +117,18 @@ function createTable()
         {
 			if(j==0){
 				var tempValue = document.getElementById("s"+(i+1)).value;
-				tbody += '<td>' + tempValue + '</td>';
+				if(tempValue == startStateValue)
+				{
+					tbody += '<td>' + '<span style="font-size:20px;">&#8594;</span>'+ tempValue + '</td>';
+				}
+				else if(finalStateValue.includes(tempValue))
+				{
+					tbody += '<td>' + '*' + tempValue + '</td>';
+				}
+				else
+				{
+					tbody += '<td>' + tempValue + '</td>';
+				}
 				stateValue.push(tempValue);
 			}
 			else{
@@ -63,10 +139,11 @@ function createTable()
     }
     var tfooter = '</table>';
 
-	var button = '<input name="generate" type="button" value="Convert to RG!" onclick="convertToRG()"/>\n'
+	var button = '<input name="generate" type="button" value="Convert to RG" onclick="convertToRG()"/>\n'
     document.getElementById('NFATable').innerHTML = theader + tbody + tfooter + button;
 
 }
+
 var alphabetNum = 1;
 function appendAlphabet() 
 {
@@ -87,4 +164,27 @@ function appendState()
 	inputState.setAttribute("size", "1");
 	inputState.setAttribute("id", "s"+stateNum);
 	document.getElementById('stateContainer').appendChild(inputState);
+}
+
+var finalStateNum = 1;
+function appendFinalState() 
+{
+	finalStateNum+=1;
+	var finalState = document.createElement("INPUT");
+	finalState.setAttribute("type", "text");
+	finalState.setAttribute("size", "1");
+	finalState.setAttribute("id", "finalState"+finalStateNum);
+	document.getElementById('finalContainer').appendChild(finalState);
+}
+
+var checkStringNum = 1;
+function appendCheckStringInput()
+{
+	checkStringNum+=1;
+	var checkString = document.createElement("INPUT");
+	checkString.setAttribute("type", "text");
+	checkString.setAttribute("id", "checkString"+checkStringNum);
+	var breakLine = document.createElement("BR");
+	document.getElementById('checkStringContainer').appendChild(checkString);
+	document.getElementById('checkStringContainer').appendChild(breakLine);
 }
