6cb972ce813ce733d1bb69fd530d0416ac89c6e3
MuhdSyafeeq <fauzisyafeeq@gmail.com>
2020-10-04 00:48:29 +0800

    Fixed Epsilon
    ~ EEEEEEEE

--- js/RegularToNFA.js
+++ js/RegularToNFA.js
@@ -80,7 +80,7 @@ function displayTable()
 				getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextInput"+alphaCount).value;
 				
 				console.log("STORING DATA >>> State" + stateCount + "nextInput" + alphaCount + " ---> " + getAlphas.innerHTML);
-				if(getAlphas.innerHTML == "" || getAlphas == "e")
+				if(getAlphas.innerHTML == "" || getAlphas.innerHTML == "e")
 				{
 					inputStateCorrespondingAlphabet.push("ε");
 				}
@@ -156,10 +156,11 @@ function displayTable()
 			
 			if(!TableDataAlphabets.includes(getAlphas.innerHTML) && (getAlphas.innerHTML != null && getAlphas.innerHTML != "" && getAlphas.innerHTML != "e"))
 			{
-				if(dataScanned == true)
+				if(dataScanned == true )
 				{
 					TableDataAlphabets.push(getAlphas.innerHTML);
 				}
+				
 			}
 			else
 			{
