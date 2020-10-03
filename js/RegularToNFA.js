9b9facadfc82e88decb9d2dc21a080137d77da54
Arun Prasath <arunprasath218@gmail.com>
2020-10-03 19:08:06 +0800

    Comple convert RG to NFA

--- js/RegularToNFA.js
+++ js/RegularToNFA.js
@@ -1,5 +1,12 @@
 var DataAlphabets = [];
+var finalStateValue=[];
+
 var TableDataAlphabets = [];
+var State =[];
+
+var inputStateArray = [];
+var inputStateCorrespondingAlphabet = [];
+var inputStateCorrespondingState = [];
 
 function displayTable()
 {
@@ -19,6 +26,7 @@ function displayTable()
 	{
 		var getState = document.createElement("text");
 		getState.innerHTML = document.getElementById("State"+stateCount).value;
+		State.push(getState.innerHTML);
 		
 		if(getState.innerHTML != "")
 		{
@@ -40,6 +48,8 @@ function displayTable()
 							var getAlphas = document.createElement("text");
 							getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextStates"+getNextInput).value;
 							tbody1 += '<td>' + getAlphas.innerHTML + '</td>';
+							inputStateArray.push(getAlphas.innerHTML);
+							inputStateCorrespondingState.push(getState.innerHTML);
 						}
 					}
 					
@@ -65,6 +75,8 @@ function displayTable()
 							var getAlphas = document.createElement("text");
 							getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextStates"+getNextInput).value;
 							tbody1 += '<td>' + getAlphas.innerHTML + '</td>';
+							inputStateArray.push(getAlphas.innerHTML);
+							inputStateCorrespondingState.push(getState.innerHTML);
 						}
 					}
 				
@@ -88,6 +100,7 @@ function displayTable()
 				getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextInput"+alphaCount).value;
 				
 				console.log("State" + stateCount + "nextInput" + alphaCount + " ---> " + getAlphas.innerHTML);
+				inputStateCorrespondingAlphabet.push(getAlphas.innerHTML);
 				dataScanned = true;
 			}
 			
@@ -108,6 +121,8 @@ function displayTable()
 		
 		var getState = document.createElement("text");
 		getState.innerHTML = document.getElementById("FinalState"+FinalstateCount).value;
+		State.push(getState.innerHTML);
+		finalStateValue.push(getState.innerHTML);
 		
 		if(getState.innerHTML != " ")
 		{
@@ -130,6 +145,11 @@ function displayTable()
 					var getAlphas = document.createElement("text");
 					getAlphas.innerHTML = document.getElementById("finalState"+FinalstateCount+"NextInput"+FinalalphaCount).value;
 					console.log("finalState"+FinalstateCount+"NextInput"+FinalalphaCount+" ---> "+getAlphas.innerHTML);
+					var tempNextState = document.createElement("text");
+					tempNextState.innerHTML = document.getElementById("finalState"+FinalstateCount+"NextState"+FinalalphaCount).value;
+					inputStateArray.push(tempNextState.innerHTML);
+					inputStateCorrespondingAlphabet.push(getAlphas.innerHTML);
+					inputStateCorrespondingState.push(getState.innerHTML);
 					dataScanned = true;
 				}
 				catch(err)
@@ -161,6 +181,88 @@ function displayTable()
 	var tfooter = '</table>';
 	document.getElementById('NFAtable').innerHTML = theader + tbody + tbody1 + tfooter;
 	console.log("Table code "+theader + tbody + tbody1 + tfooter);
+	
+	//Arun Table...........................
+	startStateValue = State[0];
+	
+	
+    var theader = '<table border="1">\n';
+    var tbody = '';
+	
+	tbody += '<tr>';
+	tbody += '<td>' + '' + '</td>';
+	for(var i=0; i<TableDataAlphabets.length; i++)
+	{
+		var tempValue = TableDataAlphabets[i];
+		tbody += '<td>' + tempValue + '</td>';
+	}
+
+	tbody += '</tr>'
+	
+    for( var i=0; i<State.length ;i++)
+    {
+        tbody += '<tr>';
+        for( var j=0; j<TableDataAlphabets.length+1;j++)
+        {
+			if(j==0){
+				var tempValue = State[i];
+				if(tempValue == startStateValue)
+				{
+					tbody += '<td>' + '<span style="font-size:20px;">&#8594;</span>'+ tempValue + '</td>';
+				}
+				else if(finalStateValue.includes(tempValue))
+				{
+					tbody += '<td>' + '*' + tempValue + '</td>';
+				}
+				else
+				{
+					tbody += '<td>' + tempValue + '</td>';
+				}
+				//stateValue.push(tempValue);
+			}
+			else{
+				tbody += '<td>' + '<span type="text" id="RGtableOutput' + i + j + '">∅</span>' + '</td>';
+			}
+        }
+        tbody += '</tr>';
+    }
+    var tfooter = '</table>';
+
+    document.getElementById('RGToNFATable').innerHTML = theader + tbody + tfooter;
+	
+	var inputStateArrayString = "";
+	var inputStateCorrespondingAlphabetString = "";
+	var inputStateCorrespondingStateString = "";
+	var TestString = ""
+	
+	for( var j=0; j<inputStateArray.length;j++){
+		TestString += "State = " + inputStateCorrespondingState[j] + " Alphabet = " + inputStateCorrespondingAlphabet[j] + " Next State = " + inputStateArray[j] + " <br>";
+	}
+	
+	document.getElementById('TestingString').innerHTML = TestString;
+	
+
+	for( var x=0; x< inputStateCorrespondingState.length; x++){
+	for( var i=0; i<State.length ;i++)
+    {
+		if(inputStateCorrespondingState[x] == State[i]){
+
+			for( var j=0; j<TableDataAlphabets.length +1;j++)
+			{
+				if(j==0){
+
+				}
+				else{
+					if(inputStateCorrespondingAlphabet[x]==TableDataAlphabets[j-1]){
+						
+					document.getElementById("RGtableOutput"+ i + j).innerHTML = inputStateArray[x];
+				}
+				}
+			}
+		}
+	}
+	}
+	
 }
 
 function displayStates()
@@ -468,7 +570,7 @@ function addFinalAlphabet(containerID)
 			var getStates = document.createElement("INPUT");
 			getStates.setAttribute("type", "text");
 			getStates.setAttribute("size", "1");
-			getStates.setAttribute("id", "finalState"+dataCheck+"NextStates"+arrayFinalNextState);
+			getStates.setAttribute("id", "finalState"+dataCheck+"NextState"+arrayFinalNextState);
 			
 			var nodeName = document.createElement("text");
 			nodeName.innerHTML = " | ";
