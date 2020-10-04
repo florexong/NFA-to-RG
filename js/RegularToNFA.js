6d64a14cfda030a4ea6999d47098e737ab77ace5
edwinkang72 <70149577+edwinkang72@users.noreply.github.com>
2020-10-04 22:31:50 +0800

    Revert "Revert "Updated Formal Description""
    This reverts commit 1ba93819443318abf8d6bf8f54e830b92dc4696e.

--- js/RegularToNFA.js
+++ js/RegularToNFA.js
@@ -29,46 +29,22 @@ function displayTable()
 		
 		if(getState.innerHTML != "")
 		{
-			if(stateCount == 1)
-			{	
-				for(var getNextInput = 1; getNextInput <= dataKeeper; getNextInput++)
-				{
-					if(document.getElementById("State"+stateCount+"nextStates"+getNextInput) == null)
-					{ 
-						console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -!-> Skipped!");
-					}
-					else
-					{
-						var getAlphas = document.createElement("text");
-						getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextStates"+getNextInput).value;
-						
-						console.log("STORING DATA >>> State["+stateCount+"] -=> " + getState.innerHTML);
-						console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -=> " + getAlphas.innerHTML);
-						
-						inputStateArray.push(getAlphas.innerHTML);
-						inputStateCorrespondingState.push(getState.innerHTML);
-					}
-				}
-			}
-			else
+			for(var getNextInput = 1; getNextInput <= dataKeeper; getNextInput++)
 			{
-				for(var getNextInput = 1; getNextInput <= dataKeeper; getNextInput++)
+				if(document.getElementById("State"+stateCount+"nextStates"+getNextInput) == null)
+				{ 
+					console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -!-> Skipped!");
+				}
+				else
 				{
-					if(document.getElementById("State"+stateCount+"nextStates"+getNextInput) == null)
-					{ 
-						console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -!-> Skipped!");
-					}
-					else
-					{
-						var getAlphas = document.createElement("text");
-						getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextStates"+getNextInput).value;
-						
-						console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -=> " + getState.innerHTML);
-						console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -=> " + getAlphas.innerHTML);
-						
-						inputStateArray.push(getAlphas.innerHTML);
-						inputStateCorrespondingState.push(getState.innerHTML);
-					}
+					var getAlphas = document.createElement("text");
+					getAlphas.innerHTML = document.getElementById("State"+stateCount+"nextStates"+getNextInput).value;
+					
+					console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -=> " + getState.innerHTML);
+					console.log("STORING DATA >>> State["+stateCount+"]nextStates["+getNextInput+"] -=> " + getAlphas.innerHTML);
+					
+					inputStateArray.push(getAlphas.innerHTML);
+					inputStateCorrespondingState.push(getState.innerHTML);
 				}
 			}
 			console.log("STORING DATA >>> >>> >>> >>> NEXT BUFFER <<< <<< <<< <<<");
@@ -160,7 +136,8 @@ function displayTable()
 				}
 			}
 			
-			if(!TableDataAlphabets.includes(getAlphas.innerHTML) && (getAlphas.innerHTML != null && getAlphas.innerHTML != "" && getAlphas.innerHTML != "e"))
+			if(!TableDataAlphabets.includes(getAlphas.innerHTML) 
+				&& (getAlphas.innerHTML != null && getAlphas.innerHTML != "" && getAlphas.innerHTML != "e"))
 			{
 				if(dataScanned == true )
 				{
@@ -173,17 +150,21 @@ function displayTable()
 				episolunCheck = true;
 			}
 		}
-		
 	}
 	
-	if(episolunCheck == true){
+	if(episolunCheck == true && !TableDataAlphabets.includes("ε")){
 		TableDataAlphabets.push("ε");
+	}
+	
+	for(var checkTables = 0; checkTables < TableDataAlphabets.length; checkTables++)
+	{
+		console.log(TableDataAlphabets[checkTables]);
 	}
 	
 	var clearMemory = document.getElementById('NFAtable')
 	clearMemory.innerHTML = "";
-	
-	//Arun Table...........................
+
+	/*** Collaborated Table ***/
 	startStateValue = State[0];
 	
     var theader = '<table border="1">\n';
@@ -221,7 +202,7 @@ function displayTable()
 				}
 			}
 			else{
-				tbody += '<td>' + '<span type="text" id="NFAtableoutput' + i + j + '">Ø</span>' + '</td>';
+				tbody += '<td>' + '<span type="text" id="RGtableOutput' + i + j + '">Ø</span>' + '</td>';
 			}
 			
         }
@@ -260,7 +241,7 @@ function displayTable()
 					else{
 						if(inputStateCorrespondingAlphabet[x]==TableDataAlphabets[j-1])
 						{
-							document.getElementById("NFAtableoutput"+ i + j).innerHTML = inputStateArray[x];
+							document.getElementById("RGtableOutput"+ i + j).innerHTML = inputStateArray[x];
 						}
 					}
 				}
@@ -281,6 +262,7 @@ function displayTable()
 	
 	document.getElementById('TestsString').innerHTML = checkStringbody1;
 }
+
 function nonEpilson (){
 	var theader = '<table border="1">\n';
     var tbody = '';
@@ -457,11 +439,11 @@ function displayStates()
 				}
 			}
 			
-			
 			var commas = document.createElement("text");
 			commas.innerHTML = ", ";
 			
-			if(!DataAlphabets.includes(getAlphas.innerHTML) && (getAlphas.innerHTML != null && getAlphas.innerHTML != " "))
+			if(!DataAlphabets.includes(getAlphas.innerHTML) 
+				&& (getAlphas.innerHTML != null && getAlphas.innerHTML != "" && getAlphas.innerHTML != "e"))
 			{
 				if(dataScanned == true)
 				{
@@ -522,7 +504,8 @@ function displayStates()
 			var commas = document.createElement("text");
 			commas.innerHTML = ", ";
 			
-			if(!DataAlphabets.includes(getAlphas.innerHTML) && (getAlphas.innerHTML != null && getAlphas.innerHTML != ""))
+			if(!DataAlphabets.includes(getAlphas.innerHTML) 
+				&& (getAlphas.innerHTML != null && getAlphas.innerHTML != "" && getAlphas.innerHTML != "e"))
 			{
 				if(dataScanned == true)
 				{
@@ -533,6 +516,8 @@ function displayStates()
 			}
 		}
 	}
+	
+	displayTable();
 }
 
 /***
